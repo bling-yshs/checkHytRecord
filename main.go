b9332ef673a98e40cc09cd1dbbcb90509339a7cf
bling-yshs <377178599@qq.com>
2023-08-05 10:42:10 +0800

    仅显示有参考价值的数据

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "bytes"
     "encoding/json"
     "fmt"
     "io"
@@ -11,6 +10,20 @@ import (
 
     ct "github.com/daviddengcn/go-colortext"
 )
+
+type BedWarsRecord struct {
+    MvpNum    int     `json:"mvpNum"`
+    PlayNum   int     `json:"playNum"`
+    WinRate   float64 `json:"winRate"`
+    BeddesNum int     `json:"beddesNum"`
+    KillDead  float64 `json:"killDead"`
+}
+
+type SkyWarsRecord struct {
+    KillNum float64 `json:"killNum"`
+    PlayNum int     `json:"playNum"`
+    WinRate float64 `json:"winRate"`
+}
 
 func main() {
     for {
@@ -29,7 +42,7 @@ func main() {
     }
 }
 
-func getRecord(questUrl string, type1 string, id string) {
+func getRecord(questUrl string, gameType string, id string) {
     resp, err := http.Get(questUrl)
     if err != nil {
         log.Fatal(err)
@@ -38,14 +51,31 @@ func getRecord(questUrl string, type1 string, id string) {
     if resp.StatusCode != http.StatusNotFound {
         b, _ := io.ReadAll(resp.Body)
         ct.Foreground(ct.Green, true)
-        fmt.Println("查询成功，玩家 " + id + " 的 " + type1 + " 战绩如下：")
+        fmt.Println("查询成功，玩家 " + id + " 的 " + gameType + " 战绩如下：")
         ct.ResetColor()
-        var formattedJSON bytes.Buffer
-        err := json.Indent(&formattedJSON, b, "", "  ")
-        if err != nil {
-            log.Fatal("JSON格式化失败:", err)
+
+        switch gameType {
+        case "起床战争":
+            var record BedWarsRecord
+            if err := json.Unmarshal(b, &record); err != nil {
+                log.Fatal("JSON解析失败:", err)
+            }
+            fmt.Printf("总场次：%d\n", record.PlayNum)
+            winNum := (float64)(record.PlayNum) * record.WinRate
+            fmt.Printf("胜场/胜率：%d/%d%%\n", (int)(winNum), int(record.WinRate*100))
+            mvpRate := float64(record.MvpNum) / (winNum) * 100
+            fmt.Printf("MVP次数/MVP率：%d/%.2f%%\n", record.MvpNum, mvpRate)
+            fmt.Printf("K/D：%.2f\n", record.KillDead)
+            fmt.Printf("破坏的床的数量：%d\n", record.BeddesNum)
+        case "空岛战争":
+            var record SkyWarsRecord
+            if err := json.Unmarshal(b, &record); err != nil {
+                log.Fatal("JSON解析失败:", err)
+            }
+            fmt.Printf("总场次：%d\n", record.PlayNum)
+            fmt.Printf("胜率：%.2f\n", record.WinRate)
+            fmt.Printf("击杀数：%.2f\n", record.KillNum)
         }
-        fmt.Println(formattedJSON.String())
     } else {
         ct.Foreground(ct.Red, true)
         fmt.Println("未查询到战绩，此 ID 可能是新玩家")
